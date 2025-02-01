d31ceb0cc8b95f2c4cfd10e107d990acc5832653
cafesmire <cafesmire@gmail.com>
2025-02-01 11:22:59 -0500

    saving...computer dying

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,20 @@
-export default function Sidebar(){
-  
-  return(
-    <div className='absolute top-9 left-8 flex flex-col p-10 rounded-sm border-2 border-gray-200 bg-zinc-400 gap-30'>
-      <h1>Welcome!</h1>
-      <div>
-        <h1>About Me</h1>
-      </div>
-      <div>
-        <h1>Projects</h1>
-      </div>
-      <div>
-        <h1>Contact</h1>
-      </div>
-    </div>
+import { Link } from "react-router-dom";
 
-  )
+export default function Sidebar() {
+  return (
+    <div className="absolute top-10 flex flex-col p-2 gap-2 text-center rounded-sm border-2 border-gray-200 bg-zinc-400">
+      <h1 className="border-b-2">Welcome</h1>
+      <ul>
+        <li>
+          <Link to="about">About Me</Link>
+        </li>
+        <li>
+          <Link to="projects">Projects</Link>
+        </li>
+        <li>
+          <Link to='contact'>Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import Navigation from './components/Navigation.jsx'
 import Footer from './components/Footer.jsx'
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+  },
+  {
+    path: 'about'
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='flex flex-col min-h-screen'>
