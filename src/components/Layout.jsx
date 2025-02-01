60bb1053d0b36100fe7c1ff1813a288135f7441d
cafesmire <cafesmire@gmail.com>
2025-02-01 17:45:27 -0500

    Change small details
    Changed a few small details like hiding overflow and having the main
    content flex to fill the remaining space

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,10 +6,11 @@ import Footer from "./Footer";
 
 export default function Layout() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div className="min-h-screen flex flex-col bg-amber-900">
+    <div className="min-h-screen flex flex-col">
       <Navigation handleClick={() => setSidebarOpen((prev) => !prev)} />
-      <div className={`relative flex flex-col flex-1 bg-stone-700`}>
+      <div className={`relative flex flex-col flex-1 overflow-hidden`}>
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <div className={`flex flex-col flex-1 transition-transform duration-300 ${sidebarOpen ? 'translate-x-25':'translate-x-0'}`}>
           <Outlet />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,21 +6,16 @@ export default function Sidebar({ sidebarOpen, setSidebarOpen }) {
     <aside>
       <div
         className={`absolute top-0 left-0 flex flex-col min-h-full w-25 text-center p-4 bg-zinc-500 transform-translate duration-300 ${
-          sidebarOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-      >
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"}`}>
         <h1 className="border-b-2">Welcome</h1>
-        <ul className="flex flex-col gap-6 text-sm">
+        <ul className="basis-50 flex flex-col justify-around text-sm">
           <li>
             <Link to="/about" onClick={() => setSidebarOpen((prev) => !prev)}>
               About Me
             </Link>
           </li>
           <li>
-            <Link
-              to="/projects"
-              onClick={() => setSidebarOpen((prev) => !prev)}
-            >
+            <Link to="/projects" onClick={() => setSidebarOpen((prev) => !prev)}>
               Projects
             </Link>
           </li>
