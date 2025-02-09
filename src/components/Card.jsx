58b4b68cd29d9e9a4214c489703f77acd4ed60b1
cafesmire <cafesmire@gmail.com>
2025-02-09 12:38:44 -0500

    reverting back to old

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,7 @@
-export default function Card({ rotationAmount, name, image }) {
-  return (
-    <div
-      className={`absolute top-[20%] left-[calc(50%-80px)] h-fit w-30 border-1 rounded-xl text-center bg-zinc-700  ${
-        rotationAmount > 0 ? "-z-10" : "z-0"
-      }`}
-      style={{
-        transform: `rotateY(${rotationAmount}deg) translateZ(200px)`,
-        transformOrigin: "center center",
-      }}
-    >
-      <h1>{name}</h1>
-      <img src={image} alt="" className="rounded-xl" />
+export default function Card(){
+  return(
+    <div>
+      <h1>Card</h1>
     </div>
-  );
+  )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,64 +1,9 @@
-import { useEffect, useState } from "react";
-import Card from "../components/Card";
-import image1 from "../assets/images/image1.jpg";
-import image2 from "../assets/images/image2.jpg";
-import image3 from "../assets/images/image3.jpg";
-import image4 from "../assets/images/image4.jpg";
+import Card from '../components/Card'
 
-const images = [image1, image2, image3, image4];
-const repoProjects = ["Flappy", "Resume", "Todo", "Menu"];
-const rotation = repoProjects.map(
-  (_, idx) => Math.floor(360 / repoProjects.length) * idx
-);
-
-export default function Projects() {
-  const [projects, setProjects] = useState(repoProjects);
-
-
-  function handleClick(e){
-	const previousProject = () => {
-		const arr = projects.copyWithin();
-		const swap = arr.shift();
-		arr.push(swap);
-		setProjects(arr);
-	}
-	const nextProject = () => {
-		const arr = projects.copyWithin();
-		const swap = arr.pop();
-		arr.unshift(swap);
-		setProjects(arr);
-	}
-	e.target.className.includes('previous') ? previousProject() : nextProject();
-  }
-
-  return (
-    <div className="flex flex-col flex-1">
-      {projects && (
-        <div className="relative flex-1 perspective-distant">
-          <button
-            className="previous absolute top-[calc(50%-5px)] left-10 size-8 rounded-full bg-zinc-500 border-1 cursor-pointer"
-            onClick={handleClick}
-          >
-            &lt;
-          </button>
-          {projects.map((item, idx) => {
-            return (
-              <Card
-                key={idx}
-                name={item}
-                rotationAmount={rotation[idx]}
-                image={images[idx]}
-              />
-            );
-          })}
-          <button
-            className="next absolute top-[calc(50%-5px)] right-10 size-8 rounded-full bg-zinc-500 border-1 cursor-pointer"
-            onClick={handleClick}
-          >
-            &gt;
-          </button>
-        </div>
-      )}
+export default function Projects(){
+  return(
+    <div>
+      <Card/>
     </div>
-  );
+  )
 }
