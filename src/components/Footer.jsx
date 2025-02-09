40b6851c77dd13be1ecb87533f548a796592d10b
cafesmire <cafesmire@gmail.com>
2025-02-09 16:42:28 -0500

    Add functionality to the carousel.
    Added functionality to the carousel. Now the left and right buttons will
    turn the carousel, applying the proper rotation, index, and image to
    each card while turning. That was an issue earlier that caused me to
    start over. Now that we're back on track, handle the transitions.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
-export default function Footer(){
-  return(
-    <footer className="flex justify-evenly text-md">
-      <p>cafesmire@gmail.com</p>
-      <p>Full Stack Engineer</p>
-      <p>Chattanooga, TN</p>
-    </footer>
+import { Link } from 'react-router-dom';
+import github from '../assets/images/github.svg';
 
-  )
+export default function Footer() {
+	return (
+		<footer className="flex justify-evenly text-md">
+			<p>cafesmire@gmail.com</p>
+			<div className='border-2 rounded-full p-1'>
+        <Link to="https://github.com/cafesmire" target="_blank">
+          <img src={github} alt="" className="invert size-6" />
+        </Link>
+      </div>
+			<p>Chattanooga, TN</p>
+		</footer>
+	);
 }
